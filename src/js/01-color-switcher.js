5dc87bc69d25ea9c49eb9541e12e7798ed37f8a8
Vitalii <105589792+Vitalii-Volianyk@users.noreply.github.com>
2022-10-12 11:24:52 +0300

    Initial

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
-
+(() => {
+	const Start = document.querySelector("[data-start]");
+	const Stop = document.querySelector("[data-stop]");
+	let intervalId;
+	Start.addEventListener("click", () => {
+		intervalId = setInterval(() => {
+			document.body.style.backgroundColor = getRandomHexColor();
+		}, 1000);
+		Start.toggleAttribute("disabled");
+		Stop.toggleAttribute("disabled");
+	});
+	Stop.addEventListener("click", () => {
+		clearInterval(intervalId);
+		Start.toggleAttribute("disabled");
+		Stop.toggleAttribute("disabled");
+	});
+	function getRandomHexColor() {
+		return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	}
+})();
